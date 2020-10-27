f6b656e90931cc9817068eadf9c1e50148dc453c
sahana40 <62323807+sahana40@users.noreply.github.com>
2020-10-27 19:01:58 +0530

    update1

--- karatsuba_multiplication.py
+++ karatsuba_multiplication.py
@@ -1,11 +1,11 @@
-def karatsuba(x, y):
+def karatsuba(p, q):
     """Performs Karatsuba multiplication on two numbers."""
     # Calculate the length of the two integers and take the minimum of the two.
-    n = min(len(str(x)), len(str(y)))
+    n = min(len(str(p)), len(str(q)))
 
     # Perform simple multiplication if any of the two numbers is less than 10.
     if n == 1:
-        return x * y
+        return p * q
 
     # Calculate half length and the power of 10 raised to that half length.
     half = n // 2
@@ -14,10 +14,10 @@ def karatsuba(x, y):
     # Divide and conquer.
     # Here, x = (a * power_of_10) + b
     # Here, y = (c * power_of_10) + d
-    a = x // power_of_10
-    b = x % power_of_10
-    c = y // power_of_10
-    d = y % power_of_10
+    a = p // power_of_10
+    b = p % power_of_10
+    c = q // power_of_10
+    d = q % power_of_10
 
     # 3 recursive calls to calculate (a * c), (b * d) and ((a + b) * (c + d)).
     ac = karatsuba(a, c)
@@ -36,10 +36,10 @@ def main():
     # x = 3141592653589793238462643383279502884197169399375105820974944592
     # y = 2718281828459045235360287471352662497757247093699959574966967627
 
-    x = input("Enter first number: ")
-    y = input("Enter second number: ")
+    p = input("Enter first number: ")
+    q = input("Enter second number: ")
 
-    result = karatsuba(int(x), int(y))
+    result = karatsuba(int(p), int(q))
     print(result)
 
 
